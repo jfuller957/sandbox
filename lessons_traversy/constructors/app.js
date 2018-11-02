f283ade7ee3bdae76c96ae03d849de85b0889e22
James Fuller <jfuller957@gmail.com>
2018-11-02 15:09:59 -0700

    day 2 learning from Traversy Media

--- lessons_traversy/constructors/app.js
+++ lessons_traversy/constructors/app.js
@@ -1,8 +1,9 @@
 // Person Constuctor
-function Person() {
-  this.name = 'James';
+function Person(name, dob) {
+  this.name = name;
+  // this.age = age;
+  this.birthday = new Date(dob);
 }
 
-const james = new Person();
-
-console.log(james);
+// const james = new Person('James', 34);
+// console.log(james.age);
