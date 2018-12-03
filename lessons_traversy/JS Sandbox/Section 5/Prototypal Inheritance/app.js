fe4be9f3d137dd17bdee1d6c7ef7f106a053041d
James Fuller <jfuller957@gmail.com>
2018-12-03 15:35:41 -0800

    prototypal inheritance complete

---
+++ lessons_traversy/JS Sandbox/Section 5/Prototypal Inheritance/app.js
@@ -0,0 +1,42 @@
+// Person Constructor
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+// Greeting
+Person.prototype.greeting = function() {
+  return `Hello there ${this.firstName} ${this.lastName}`;
+};
+
+const person1 = new Person('John', 'Doe');
+
+// console.log(person1.greeting());
+
+// Customer Constructor
+function Customer(firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+
+  this.phone = phone;
+  this.membership = membership;
+}
+
+// Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+// Make Customer.prototype return customer
+Customer.prototype.constructor = Customer;
+
+// Create Customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+
+// Customer Greeting
+Customer.prototype.greeting = function() {
+  return `Hello there ${this.firstName} ${
+    this.lastName
+  } welcome to our company`;
+};
+
+console.log(customer1.greeting());
