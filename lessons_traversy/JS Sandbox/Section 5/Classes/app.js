7065f7832109d127c84190257627552b32469aac
James Fuller <jfuller957@gmail.com>
2018-12-04 18:53:01 -0800

    classes lesson 12_4

--- lessons_traversy/JS Sandbox/Section 5/Classes/app.js
+++ lessons_traversy/JS Sandbox/Section 5/Classes/app.js
@@ -1,8 +1,33 @@
 class Person {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getsMarried(newLastName) {
+    this.lastName = newLastName;
+  }
+
+  static addNumbers(x, y) {
+    return x + y;
   }
 }
 
-const christina = new Person('Christina', '');
+const christina = new Person('Christina', 'Vanderwerken', '11-14-1984');
+
+christina.getsMarried('Fuller');
+
+console.log(christina);
+
+console.log(Person.addNumbers(1, 2));
