99aed72081e03882a086ecd11425c70c5807e054
James Fuller <jfuller957@gmail.com>
2018-12-06 19:05:14 -0800

    easyHTTP2 es6 arrowfunctions fetch api

---
+++ lessons_traversy/js_sandbox/section_seven/easyhttp_library_fetch_promise/app.js
@@ -0,0 +1,29 @@
+const http = new EasyHTTP;
+
+// Get Users
+
+// const users = http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// User Data
+const data = {
+  name: 'James Fuller',
+  username: 'jamesfuller',
+  email: 'jfuller@none.com'
+}
+
+// Create User
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Create Put/Update User
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Delete User
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
