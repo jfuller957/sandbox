8203bd3d09aeafb5bccca50dd14772145b4f2f58
James Fuller <jfuller957@gmail.com>
2018-12-08 14:01:14 -0800

    regex notes continued

--- lessons_traversy_media/modern_js_from_beginning/section_nine/regular_expressions/regex_test/app.js
+++ lessons_traversy_media/modern_js_from_beginning/section_nine/regular_expressions/regex_test/app.js
@@ -12,8 +12,27 @@ re = /h*llo/i;      // Matches any character 0 or more times
 re = /gre?a?y/i;    // ? Optional Character
 re = /gre?a?y\?/i;  // Escape Characters
 
+// Brackets [] - Character Set
+re = /gr[ae]y/i;    // Must be an a or e.  This is a better choice than /gre?a?y/i; because brackets will go null if the characters aren't acceptable.
+re = /[GF]ray/;     // Must be a G or F not g or f or other character
+re = /[^GF]ray/;    // Must be anything except G or F
+re = /[A-Z]ray/;    // Match any Uppercase Letter
+re = /[a-z]ray/;    // Match any Lowercase Letter
+re = /[A-Za-z]ray/; // Match any Letter
+re = /[0-9]ray/;    // Match any Digit
+re = /[0-9][0-9]ray/;    // Match any Lowercase Letter
+
+// Brace {} - Quantifiers
+re = /Hel{2}o/i;    // The letter preceeding the {} must occur exactly {m} times.
+re = /Hel{2,4}o/i;  // The letter preceeding the {} must occur 2 to 4 times.
+
+re = /Hel{2,}o/i;   // The letter preceeding the {} must occur at least {m,} times.
+
+// Parentheses () - Grouping
+re = /([0-9]x){3}/;
+
 // String to match
-const str = 'Gray?';
+const str = '3x3x3x';
 
 // Log Results
 const result = re.exec(str);
