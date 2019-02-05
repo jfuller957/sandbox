69cd11dbe1d210835558dd1df674399847dd2756
James Fuller <jfuller957@gmail.com>
2019-02-05 13:49:58 -0800

    removed placeholder items, hide list if empty else show

--- lessons_traversy_media/modern_js_from_beginning/section_twelve/tracalorie/app.js
+++ lessons_traversy_media/modern_js_from_beginning/section_twelve/tracalorie/app.js
@@ -12,9 +12,9 @@ const ItemCtrl = (function () {
   // Data Structure / State
   const data = {
     items: [
-      { id: 0, name: 'Steak Dinner', calories: 1200 },
-      { id: 1, name: 'Cookie', calories: 400 },
-      { id: 2, name: 'Eggs', calories: 300 }
+      // { id: 0, name: 'Steak Dinner', calories: 1200 },
+      // { id: 1, name: 'Cookie', calories: 400 },
+      // { id: 2, name: 'Eggs', calories: 300 }
     ],
     currentItem: null,
     totalCalories: 0
@@ -84,6 +84,8 @@ const UICtrl = (function () {
       }
     },
     addListItem: function (item) {
+      // Show the list
+      document.querySelector(UISelectors.itemList).style.display = 'block';
       // Create li element
       const li = document.createElement('li');
       // Add class
@@ -101,6 +103,9 @@ const UICtrl = (function () {
     clearInput: function () {
       document.querySelector(UISelectors.itemNameInput).value = '';
       document.querySelector(UISelectors.itemCaloriesInput).value = '';
+    },
+    hideList: function () {
+      document.querySelector(UISelectors.itemList).style.display = 'none';
     },
     getSelectors: function () {
       return UISelectors;
@@ -145,6 +150,14 @@ const App = (function (ItemCtrl, UICtrl) {
     init: function () {
       // Fetch items from data structure
       const items = ItemCtrl.getItems();
+
+      // Check if any items
+      if (items.length === 0) {
+        UICtrl.hideList();
+      } else {
+        // Populate list with items
+        UICtrl.populateItemList(items);
+      }
 
       // Populate list with items
       UICtrl.populateItemList(items);
