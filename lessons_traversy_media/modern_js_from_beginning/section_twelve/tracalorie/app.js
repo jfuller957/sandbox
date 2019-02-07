0f2d87790180ae7befeefded00a193b6fb7a8477
James Fuller <jfuller957@gmail.com>
2019-02-06 18:30:02 -0800

    loop to add calorie totals

--- lessons_traversy_media/modern_js_from_beginning/section_twelve/tracalorie/app.js
+++ lessons_traversy_media/modern_js_from_beginning/section_twelve/tracalorie/app.js
@@ -45,6 +45,20 @@ const ItemCtrl = (function () {
 
       return newItem;
     },
+    getTotalCalories: function () {
+      let total = 0;
+
+      // Loop through items and add calories
+      data.items.forEach(function (item) {
+        total += item.calories;
+      });
+
+      // Set total calories in data structure
+      data.totalCalories = total;
+
+      // Return total
+      return data.totalCalories;
+    },
     logData: function () {
       return data;
     }
@@ -57,7 +71,8 @@ const UICtrl = (function () {
     itemList: '#item-list',
     addBtn: '.add-btn',
     itemNameInput: '#item-name',
-    itemCaloriesInput: '#item-calories'
+    itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories',
   }
 
   // Public Methods
@@ -107,6 +122,9 @@ const UICtrl = (function () {
     hideList: function () {
       document.querySelector(UISelectors.itemList).style.display = 'none';
     },
+    showTotalCalories: function (totalCalories) {
+      document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+    },
     getSelectors: function () {
       return UISelectors;
     }
@@ -137,6 +155,13 @@ const App = (function (ItemCtrl, UICtrl) {
 
       // Add item to UI list
       UICtrl.addListItem(newItem);
+
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+
 
       // Clear fields
       UICtrl.clearInput();
