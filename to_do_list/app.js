8350cd07ee2c56248760a31912a584498430cbae
James Fuller <jfuller957@gmail.com>
2019-03-08 17:55:02 -0800

    to do list change 1

--- to_do_list/app.js
+++ to_do_list/app.js
@@ -1,1 +1,18 @@
- let todos = ['item1', 'item2', 'item3'];
+/*
+It should ...
+
+It should have a place to store todos.
+It should have a way to display todos.
+It should have a way to add new todos.
+It should have a way to change a todo.
+It should have a way to delete a todo.
+
+*/
+
+let todos = ["item1", "item2", "item3"];
+
+// todos.push("item4");
+
+todos[0] = "Item 1 updated";
+
+console.log(todos);
