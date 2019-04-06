0657b89f6c429e1014230f1ad80772b51f1f6cf7
James Fuller <jfuller957@gmail.com>
2019-04-06 15:10:23 -0700

    forEach play

--- full_stack_journey/js/foreach.js
+++ full_stack_journey/js/foreach.js
@@ -1,7 +1,39 @@
-const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+function forEach(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i, arr);
+  }
+}
 
-console.log(days[0]);
+// const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-days.forEach(function(day, index) {
-  console.log(`${index + 1} day of the week -- ${day}`);
+// console.log(days[0]);
+
+// days.forEach(function(day, index) {
+//   console.log(`${index + 1} day of the week -- ${day}`);
+// });
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+function double(arr) {
+  for (let i = 0; i < arr.length; i++) console.log(arr[i] * 2);
+}
+
+double(arr);
+
+forEach(arr, function(number) {
+  console.log(number * 2);
 });
+
+let strings = ["my", "forEach", "example"];
+
+let result = "";
+
+forEach(strings, function(str, index, array) {
+  if (array.length - 1 !== index) {
+    result += str + " ";
+  } else {
+    result += str + "!!!";
+  }
+});
+
+console.log(result);
