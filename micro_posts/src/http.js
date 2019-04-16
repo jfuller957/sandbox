5597350d5ee808cdc44636972011bfd2b563743e
James Fuller <jfuller957@gmail.com>
2019-04-15 21:33:02 -0700

    add posts, alert success, clear alerts

--- micro_posts/src/http.js
+++ micro_posts/src/http.js
@@ -3,13 +3,13 @@
  * Library for making HTTP requests
  *
  * @version 3.0.0
- * @author  James Fuller
+ * @author  Brad Traversy
  * @license MIT
  *
  **/
 
 class EasyHTTP {
-  // Make an HTTP GET Request
+  // Make an HTTP GET Request 
   async get(url) {
     const response = await fetch(url);
     const resData = await response.json();
@@ -19,27 +19,28 @@ class EasyHTTP {
   // Make an HTTP POST Request
   async post(url, data) {
     const response = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-type": "application/json"
+        'Content-type': 'application/json'
       },
       body: JSON.stringify(data)
     });
 
     const resData = await response.json();
     return resData;
+   
   }
 
-  // Make an HTTP PUT Request
-  async put(url, data) {
+   // Make an HTTP PUT Request
+   async put(url, data) {
     const response = await fetch(url, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-type": "application/json"
+        'Content-type': 'application/json'
       },
       body: JSON.stringify(data)
     });
-
+    
     const resData = await response.json();
     return resData;
   }
@@ -47,15 +48,18 @@ class EasyHTTP {
   // Make an HTTP DELETE Request
   async delete(url) {
     const response = await fetch(url, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        "Content-type": "application/json"
+        'Content-type': 'application/json'
       }
     });
 
-    const resData = await "Resource Deleted...";
+    const resData = await 'Resource Deleted...';
     return resData;
   }
-}
 
-export const http = new EasyHTTP();
+ }
+
+ export const http = new EasyHTTP();
+
+ 
